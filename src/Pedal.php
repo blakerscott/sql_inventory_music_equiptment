a4b2968eb8a44f0cb7b63afe7427b37b91ceebae
Blake Scott and Jason Awbrey <blakerscott@gmail.com>
2016-02-22 15:33:31 -0800

    add more things

---
+++ src/Pedal.php
@@ -0,0 +1,124 @@
+<?php
+
+
+	 class Pedal
+		{
+		private $brand;
+		private $model;
+		private $type;
+		private $batteries;
+		private $rating;
+		private $id;
+
+		function __construct($brand, $model, $type, $batteries, $rating, $id = NULL)
+		{
+			$this->brand = $brand;
+			$this->model = $model;
+			$this->type = $type;
+			$this->batteries = $batteries;
+			$this->rating = $rating;
+			$this->id = $id;
+		}
+
+		function getBrand()
+		{
+			return $this->brand;
+		}
+
+		function setBrand($brand)
+		{
+			$this->brand = $brand;
+		}
+		function getModel()
+		{
+			return $this->model;
+		}
+
+		function setModel($model)
+		{
+			$this->model = $model;
+		}
+		function getType()
+		{
+			return $this->type;
+		}
+
+		function set($type)
+		{
+			$this->type = $type;
+		}
+		function getBatteries()
+		{
+			return $this->batteries;
+		}
+
+		function setBatteries($batteries)
+		{
+			$this->batteries = $batteries;
+		}
+		function getRating()
+		{
+			return $this->rating;
+		}
+
+		function setRating()
+		{
+			$this->rating = $rating;
+		}
+		function getID()
+		{
+			return $this->id;
+		}
+
+		function setID($id)
+		{
+			$this->id = $id;
+		}
+
+		function save()
+		{
+			$GLOBALS['db']->exec("INSERT INTO pedals (brand, model, type, batteries, rating) VALUES ('{$this->brand}', '{$this->model}', '{$this->type}','{$this->batteries}','{$this->rating}');");
+			$this->id = $GLOBALS['db']->lastInsertId();
+		}
+
+
+		static function getAll()
+		{
+			$returned_pedals = $GLOBALS['db']->query('SELECT * FROM pedals;');
+			$pedals = array();
+			foreach($returned_pedals as $pedal) {
+			  $brand = $pedal['brand'];
+			  $model = $pedal['model'];
+			  $type = $pedal['type'];
+			  $batteries = $pedal['batteries'];
+			  $rating = $pedal['rating'];
+			  $id = $pedal['id'];
+			  $new_pedal = new Pedal($brand, $model, $type, $batteries, $rating, $id);
+			  array_push($pedals, $new_pedal);
+			}
+			return $pedals;
+
+		}
+
+		static function deleteAll()
+		{
+			$GLOBALS['db']->exec("DELETE FROM pedals;");
+		}
+
+		function find($keyword)
+		{
+			$pedals = Pedal::getAll();
+			$result = array();
+			foreach($pedals as $key => $pedal)
+			{
+				if(strpos($pedal[$key], $keyword))
+				{
+					array_push($result, $pedal);
+				}
+			}
+			return $result;
+		}
+
+
+	}
+ ?>
---
+++ tests/PedalTest.php
@@ -0,0 +1,82 @@
+<?php
+
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
+
+	require_once 'src/Pedal.php';
+
+	// This error reporting shit is all about being able to see verbose error messages
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+
+	// This is from Jason Awbrey. This is how I decided to structure
+	// Our connection to the database, because if forwhatever reason
+	// The connection doesn't work then I want the user to see
+	// Whatever error message is available, instead of just not
+	// Having the application work.
+	// This is known as a try catch block and is a useful code architecture
+	// For attmpting to run code, and if it fails then having something
+	// Specific happen... like printing an error message, or sending the user
+	// To a different page.
+
+
+		// try {
+		//    $host = 'mysql:host=localhost;dbname=music_equipment_test';
+		//    $username = 'root';
+		//    $password = 'root';
+		//    $db = new PDO($host, $username, $password);
+		// 	$db->exec("SET NAMES 'utf8'");
+		// 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// } catch(Exception $e){
+		// 	echo 'Sorry, there seems to be a problem connecting to the database. Here is all we know:';
+		// 	echo $e->getMessage();
+		// 	die();
+		// }
+ // End of jason's rant
+
+
+ $host = 'mysql:host=localhost;dbname=music_equipment_test';
+ $username = 'root';
+ $password = 'root';
+ $db = new PDO($host, $username, $password);
+
+
+	class PedalTest extends PHPUnit_Framework_TestCase
+	{
+
+		protected function tearDown()
+		{
+			Pedal::deleteAll();
+		}
+
+		function test_save_and_getAll()
+		{
+		//Arrange
+		$test_pedal = new Pedal("Electro-Harmonix","Big Muff","Overdrive",1,4);
+
+
+		//Act
+		$test_pedal->save();
+		$result = Pedal::getAll();
+
+		//Assert
+		$this->assertEquals($test_pedal, $result[0]);
+		}
+
+		function test_find()
+		{
+			//Arrange
+			$test_pedal = new Pedal("Electro-Harmonix","Big Muff","Overdrive",1,4);
+
+			//Act
+			$test_pedal->save();
+			$result = $test_pedal->find("Big");
+
+			//Assert
+			$this->assertEquals(["Big Muff"], $result);
+		}
+	}
+
+?>
